9b7e51e280ea2bf13274eadcbc02df126d079506
Bhyland <blakehyland95@gmail.com>
2021-10-21 10:59:14 -0600

    added partials. added helpers. added testing for helpers

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const helpers = require('./utils/helpers');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: 'Super secret secret',
